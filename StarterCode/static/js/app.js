91f130830cb0080f4cc52fba3b510f802e42b019
Dalton Field <daltonfield@Daltons-MacBook-Pro.local>
2021-01-07 18:34:39 -0700

    create optionChanged function and clearing list

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,5 +7,27 @@ console.log(data.names)
 data.names.forEach(testid=>{
     dropdownvar.append("option").text(testid)
 })
+optionChanged(data.names[0])
+})
+
+//Append demographic data
+function demoinfo(testid){
+    d3.json("static/js/samples.json").then((data)=>{
+        console.log(data.metadata)
+    var demodata = d3.select("#sample-metadata")
+    var filtereddata = data.metadata.filter(md=>md.id==testid)
+    //fetching first element
+    var firstdemo = filtereddata[0]
+    //Clearing previous list
+    demodata.html('')
+    Object.entries(firstdemo).forEach(([key,value])=>{
+        demodata.append("p").text(key+":"+value)
+    })
 
 })
+
+}
+
+//Optionchanged function
+function optionChanged(input)
+{demoinfo(input)}
